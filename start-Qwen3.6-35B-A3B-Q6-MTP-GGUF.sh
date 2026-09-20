49356593f5f09bd8bff2fdc69c91f7b90e1e1b7e
Hubi <hubi@hubin>
2026-09-20 15:35:19 +0200

    Start-Skript für Qwen3.6-35B-A3B-Modell hinzufügen

---
+++ start-Qwen3.6-35B-A3B-Q6-MTP-GGUF.sh
@@ -0,0 +1,31 @@
+/home/hubi/src/llama.cpp/build/bin/llama-server -m /mnt/nvme1/temp/Qwen3.6-35B-A3B-Q6-MTP-GGUF/Qwen3.6-35B-A3B-UD-Q6_K.gguf --mmproj /mnt/nvme1/temp/Qwen3.6-35B-A3B-Q6-MTP-GGUF/mmproj-BF16.gguf \
+  --port 8080 \
+  --host 0.0.0.0 \
+  --poll 0 \
+  --parallel 1 \
+  --n-gpu-layers 999 \
+  --n-cpu-moe 36 \
+  --spec-type draft-mtp \
+  --spec-draft-n-max 3 \
+  --image-min-tokens 1024 \
+  --temperature 0.7 \
+  --cache-type-k q8_0 \
+  --cache-type-v q8_0 \
+  --reasoning-format auto \
+  --ctx-size 163840 \
+  --batch-size 512 \
+  --ubatch-size 512 \
+  --threads 8 \
+  --threads-batch 8 \
+  --flash-attn 1 \
+  --jinja \
+  --load-mode mlock
+
+# Kontextgrößen: 32768 65536 81920 102400 122880 143360 159744 163840 204800 225280 262144
+
+#   --mmproj /mnt/C42EA36B2EA354E8/temp/Qwen3.6-35B-A3B-MXFP4_MOE/mmproj-BF16.gguf \
+#   --mmproj /mnt/nvme1/temp/Qwen3.6-35B-A3B-Q6-MTP-GGUF/mmproj-BF16.gguf
+
+# \  --cache-ram 16384
+#   --n-cpu-moe 36 \
+#   --no-mmap
